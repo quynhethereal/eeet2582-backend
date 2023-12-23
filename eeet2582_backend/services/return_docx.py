37e53b7ae0aacca2edfc8ede6566b90f4596a076
Minh Phan <minh40490@gmail.com>
2023-12-23 22:04:04 +0700

    Update return_docx.py

--- eeet2582_backend/services/return_docx.py
+++ eeet2582_backend/services/return_docx.py
@@ -31,26 +31,6 @@ def create_docx():
     for heading in standalone_headings:
         doc.add_heading(heading.content, level=1)
 
-    # # The sorting mechanism depends on whether the Heading is associated with a DocumentParagraph
-    # sorted_items = []
-    # for para in paragraphs:
-    #     related_headings = headings.filter(document_paragraph=para)
-    #     for heading in related_headings:
-    #         sorted_items.append(('heading', heading))
-    #     sorted_items.append(('paragraph', para))
-
-    # # Add headings without a document_paragraph (those come before the endnotes)
-    # standalone_headings = headings.filter(document_paragraph__isnull=True)
-    # for heading in standalone_headings:
-    #     sorted_items.append(('heading', heading))
-
-    # # Add sorted headings and paragraphs to the document
-    # for item_type, item in sorted_items:
-    #     if item_type == 'heading':
-    #         doc.add_heading(item.content, level=1)  # Add as heading with level 1
-    #     elif item_type == 'paragraph':
-    #         doc.add_paragraph(item.content)
-
     # Add endnotes
     for endnote in EndNote.objects.filter(user_document=user_doc):
         doc.add_paragraph(endnote.content)
