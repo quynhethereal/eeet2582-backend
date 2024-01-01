9e242ef572f1ed64191f43dfd91f7853901b0c2d
Duc Trinh <trinhduc33@gmail.com>
2024-01-01 21:45:44 +0700

    caption

--- eeet2582_backend/api/models/heading.py
+++ eeet2582_backend/api/models/heading.py
@@ -9,3 +9,12 @@ class Heading(models.Model):
 
     def __str__(self):
         return f"{self.content}"
+    
+class Caption(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_document = models.ForeignKey('UserDocument', on_delete=models.CASCADE)
+    document_paragraph = models.ForeignKey('DocumentParagraph', on_delete=models.CASCADE, null=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return f"{self.content}"
