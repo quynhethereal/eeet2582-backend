771490e45cd76f94489591ad92484e035c1cee42
DeaLoux <42980704+dealoux@users.noreply.github.com>
2024-01-04 21:13:19 +0700

    process_docx without celery

--- eeet2582_backend/services/process_docx.py
+++ eeet2582_backend/services/process_docx.py
@@ -28,43 +28,79 @@ def correct_text(text):
 
 def process_docx():
     user_doc = UserDocument.objects.latest('created_at')
+    process_title(user_doc)
     process_paragraph(user_doc)
+    process_heading(user_doc)
+    process_endnote(user_doc)
+
+def process_title(user_doc):
+    titles = DocumentTitle.objects.filter(userdocument=user_doc).order_by('id')
+
+    for title in titles:
+        if title.title:
+            response = correct_text(title.title)
+            if response.status_code == 200:
+                corrected_text = response.json()[0].strip()
+                title.title = corrected_text
+                title.save()
+            else:
+                print("Error: ", response.status_code)
+                return
 
 def process_paragraph(user_doc):
     paragraphs = DocumentParagraph.objects.filter(user_document=user_doc).order_by('id')
 
-    paragraph = paragraphs[7]
-    if paragraph.content:
-        corrected_paragraph = ""
-        sentences = sent_tokenize(paragraph.content)
-        
-        for sentence in sentences:
-            # print(sentence)
-            response = correct_text(sentence)
+    for paragraph in paragraphs:
+        if paragraph.content:
+            corrected_paragraph = ""
+            sentences = sent_tokenize(paragraph.content)
+
+            for sentence in sentences:
+                # print(sentence)
+                response = correct_text(sentence)
+                if response.status_code == 200:
+                    corrected_text = response.json()[0].strip()
+                    # print(corrected_text)
+                    corrected_paragraph += corrected_text + " "
+                else:
+                    print("Error: ", response.status_code)
+                    return
+            # print(corrected_paragraph)
+            paragraph.content = corrected_paragraph
+            paragraph.save()
+    
+def process_heading(user_doc):
+    headings = Heading.objects.filter(user_document=user_doc).order_by('id')
+
+    for heading in headings:
+        if heading.content:
+            corrected_heading = ""
+            sentences = sent_tokenize(heading.content)
+
+            for sentence in sentences:
+                # print(sentence)
+                response = correct_text(sentence)
+                if response.status_code == 200:
+                    corrected_text = response.json()[0].strip()
+                    corrected_heading += corrected_text + " "
+                else:
+                    print("Error: ", response.status_code)
+                    return
+            heading.content = corrected_heading
+            heading.save()
+
+def process_endnote(user_doc):
+    endnotes = EndNote.objects.filter(user_document=user_doc).order_by('id')
+
+    for endnote in endnotes:
+        if endnote.content:
+            response = correct_text(endnote.content)
             if response.status_code == 200:
                 corrected_text = response.json()[0].strip()
-                # print(corrected_text)
-                corrected_paragraph += corrected_text + " "
+                endnote.content = corrected_text
+                endnote.save()
             else:
                 print("Error: ", response.status_code)
-        print(corrected_paragraph)
-
-    # for paragraph in paragraphs:
-    #     if paragraph.content:
-    #         corrected_paragraph = ""
-    #         sentences = sent_tokenize(paragraph.content)
-
-    #         for sentence in sentences:
-    #             # print(sentence)
-    #             response = correct_text(sentence)
-    #             if response.status_code == 200:
-    #                 corrected_text = response.json()[0].strip()
-    #                 # print(corrected_text)
-    #                 corrected_paragraph += corrected_text + " "
-    #             else:
-    #                 print("Error: ", response.status_code)
-    #         # print(corrected_paragraph)
-    #         paragraph.content = corrected_paragraph
-    #         paragraph.save()
+                return
 
 process_docx()
