127288048efe84d7c410091515e5e2be8cdc8603
DeaLoux <42980704+dealoux@users.noreply.github.com>
2024-01-04 21:56:18 +0700

    testing celery on process_docx

--- eeet2582_backend/services/process_docx.py
+++ eeet2582_backend/services/process_docx.py
@@ -2,6 +2,7 @@ import os
 import requests
 import django
 import nltk
+from celery import shared_task
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eeet2582_backend.settings')
 django.setup()
@@ -15,21 +16,49 @@ from eeet2582_backend.api.models.user_document import UserDocument
 from eeet2582_backend.api.models.document_table import DocumentTable
 from eeet2582_backend.api.models.table_row import TableRow
 from eeet2582_backend.api.models.row_cell import RowCell
+from eeet2582_backend.celery import app
 
 nltk.download('punkt')
 from nltk.tokenize import sent_tokenize
 
+@shared_task
 def correct_text(text):
     api_endpoint = "https://polite-horribly-cub.ngrok-free.app/generate_code"
     params = {
         'prompts': f'Correct English:{text} Here is the corrected version:'
     }
-    return requests.get(api_endpoint, params=params)
+    response = requests.get(api_endpoint, params=params)
+    if(response.status_code == 200):
+        return response.json()[0].strip()
+    else:
+        return text;
 
+@shared_task
+def correct_text_paragraph(paragraph_id):
+    paragraph = DocumentParagraph.objects.get(id=paragraph_id)
+    if paragraph.content:
+        corrected_paragraph = ""
+        sentences = sent_tokenize(paragraph.content)
+        for sentence in sentences:
+            corrected_paragraph += correct_text.delay(sentence) + " "
+        paragraph.content = corrected_paragraph
+        paragraph.save()
+
+def process_paragraph(user_doc):
+    paragraphs = DocumentParagraph.objects.filter(user_document=user_doc).order_by('id')
+
+    for paragraph in paragraphs:
+        correct_text_paragraph.delay(paragraph.id)
+
+@app.task
 def process_docx():
     user_doc = UserDocument.objects.latest('created_at')
-    process_title(user_doc)
     process_paragraph(user_doc)
+
+def process_docx_old():
+    user_doc = UserDocument.objects.latest('created_at')
+    process_title(user_doc)
+    process_paragraph_old(user_doc)
     process_heading(user_doc)
     process_endnote(user_doc)
 
@@ -38,16 +67,10 @@ def process_title(user_doc):
 
     for title in titles:
         if title.title:
-            response = correct_text(title.title)
-            if response.status_code == 200:
-                corrected_text = response.json()[0].strip()
-                title.title = corrected_text
-                title.save()
-            else:
-                print("Error: ", response.status_code)
-                return
+            title.title = correct_text(title.title)
+            title.save()
 
-def process_paragraph(user_doc):
+def process_paragraph_old(user_doc):
     paragraphs = DocumentParagraph.objects.filter(user_document=user_doc).order_by('id')
 
     for paragraph in paragraphs:
@@ -57,15 +80,8 @@ def process_paragraph(user_doc):
 
             for sentence in sentences:
                 # print(sentence)
-                response = correct_text(sentence)
-                if response.status_code == 200:
-                    corrected_text = response.json()[0].strip()
-                    # print(corrected_text)
-                    corrected_paragraph += corrected_text + " "
-                else:
-                    print("Error: ", response.status_code)
-                    return
-            # print(corrected_paragraph)
+                corrected_paragraph += correct_text(sentence) + " "
+                # print(corrected_paragraph)
             paragraph.content = corrected_paragraph
             paragraph.save()
     
@@ -78,14 +94,7 @@ def process_heading(user_doc):
             sentences = sent_tokenize(heading.content)
 
             for sentence in sentences:
-                # print(sentence)
-                response = correct_text(sentence)
-                if response.status_code == 200:
-                    corrected_text = response.json()[0].strip()
-                    corrected_heading += corrected_text + " "
-                else:
-                    print("Error: ", response.status_code)
-                    return
+                corrected_heading += correct_text(sentence) + " "
             heading.content = corrected_heading
             heading.save()
 
@@ -94,13 +103,7 @@ def process_endnote(user_doc):
 
     for endnote in endnotes:
         if endnote.content:
-            response = correct_text(endnote.content)
-            if response.status_code == 200:
-                corrected_text = response.json()[0].strip()
-                endnote.content = corrected_text
-                endnote.save()
-            else:
-                print("Error: ", response.status_code)
-                return
-
-process_docx()
+            endnote.content = correct_text(endnote.content)
+            endnote.save()
+            
+process_docx_old()
